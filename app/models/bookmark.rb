5dfe82574dc03b76686e8b0578ce679d871d8938
mrinp <mrinp@users.noreply.github.com>
2017-05-25 16:03:11 -0500

    Done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,6 @@
 class Bookmark < ApplicationRecord
 
-  validates :movie_id, :uniqueness => true
-  validates :user_id, :presence => true
+  validates :movie_id, :uniqueness => {:scope => :user_id}
 
   belongs_to :movie, :class_name => "Movie", :foreign_key => "movie_id"
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
